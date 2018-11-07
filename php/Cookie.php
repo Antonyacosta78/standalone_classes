5b79e322a90b449ad0334c979599d8cca6abcbf1
Mickael Souza <mickael.souza.if@gmail.com>
2018-11-07 21:30:46 -0200

    Created class Cookie.php for crate and managing cookies

---
+++ php/Cookie.php
@@ -0,0 +1,59 @@
+<?php  
+/* PHP Class for create and managing cookies
+ * AUTHOR: Mickael Souza
+ * LAST EDIT: 2018-11-07
+ */
+class Cookie 
+{
+	private static $time;
+
+	public function __construct()
+	{
+		self::$time = time() + 1*86400;
+	}
+
+	public static function setCookie($name, $value)
+	{
+		setcookie($name, $value, self::$time,'/');
+	}
+
+	public static function setCookies(Array $cookies)
+	{
+		foreach ($cookies as $name => $value) {
+			self::setCookie($name, $value);
+		}
+	}
+
+	public static function getCookie($name) 
+	{
+		return filter_input(INPUT_COOKIE, $name); 
+	}
+
+	public static function getCookies(Array $cookies)
+	{
+		$list_of_cookies = [];
+
+		foreach ($cookies as $name) {
+			$list_of_cookies[$name] = self::getCookie($name);
+		}
+
+		return $list_of_cookies;
+	}
+
+	public static function cookieExists($name)
+	{
+		return (filter_input(INPUT_COOKIE,$name))?true:false; 
+	}
+
+	public static function destroyCookie($name) 
+	{
+		setcookie($name, null, -1,'/');
+	}
+
+	public static function destroyCookies(Array $cookies)
+	{
+		foreach ($cookies as $name) {
+			self::destroyCookie($name);
+		}
+	}
+}
