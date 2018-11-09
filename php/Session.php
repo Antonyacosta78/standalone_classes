7204ba8cae12f255e46e8cd2abdab8a95da9fc45
Mickael Souza <mickael.souza.if@gmail.com>
2018-11-09 01:23:17 -0200

    Added getAllSessions method for list all sessions

--- php/Session.php
+++ php/Session.php
@@ -1,21 +1,21 @@
 <?php  
 /* PHP Class for create and managing sessions
  * AUTHOR: Mickael Souza
- * LAST EDIT: 2018-11-07
+ * LAST EDIT: 2018-11-08
  */
 class Session 
 {
-	public function __construct()
-	{
-		if(session_status() == PHP_SESSION_NONE) {
-			session_start();
-		}
-	}
+    public function __construct()
+    {
+        if(session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
 
-	public static function setSession($name, $value)
-	{
-		$_SESSION[$name] = $value;
-	}
+    public static function setSession($name, $value)
+    {
+        $_SESSION[$name] = $value;
+    }
 
     public static function setSessions(Array $sessions)
     {
@@ -24,19 +24,34 @@ class Session
         }
     }
 
-	public static function getSession($name)
-	{
-		return (isset($_SESSION[$name])) ? $_SESSION[$name] : false;
-	}
+    public static function getSession($name)
+    {
+        return (isset($_SESSION[$name])) ? $_SESSION[$name] : false;
+    }
 
     public static function getSessions(Array $sessions)
     {   
+        $list_of_sessions = [];
+
         foreach ($sessions as $name) {
-            self::getSession($name);
+            $list_of_sessions[$name] = self::getSession($name);
         }
+
+        return $list_of_sessions;
     }
 
-	public static function setSessionAttribute($name, $attr, $value) 
+    public static function getAllSessions()
+    {   
+        $list_of_sessions = [];
+
+        foreach ($_SESSION as $name => $value) {
+            $list_of_sessions[$name] = self::getSession($name);
+        }
+
+        return $list_of_sessions;
+    }
+
+    public static function setSessionAttribute($name, $attr, $value) 
     {
         $_SESSION[$name][$attr] = $value;
     }
@@ -51,8 +66,8 @@ class Session
         return (isset($_SESSION[$name])) ? true : false;
     }
 
-    public static function destroySession()
+    public static function destroySessions()
     {
-    	session_destroy();
+        session_destroy();
     }
 }
